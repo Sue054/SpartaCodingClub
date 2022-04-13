23629cfa1bce15bbda362c5590d004067b6d3d66
Su <101512811+Sue054@users.noreply.github.com>
2022-04-13 22:54:26 +0900

    bookreview
    - flask 시작
    - api 설계
    - robo 3t 사용하기
    - 프로젝트 만들기 연습

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
@@ -11,41 +8,31 @@ db = client.dbsparta
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
-
-@app.route('/memo', methods=['GET'])
-def listing():
-    articles = list(db.articles.find({}, {'_id': False}))
-    return jsonify({'all_articles':articles})
+    return render_template('index.html')
 
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
+@app.route('/review', methods=['POST'])
+def write_review():
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
 
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
-
-    doc={
-        'title':title,
-        'image':image,
-        'desc':desc,
-        'url':url_receive,
-        'comment':comment_receive
+    doc = {
+        'title':title_receive,
+        'author':author_receive,
+        'review':review_receive
     }
-    db.articles.insert_one(doc)
+
+    db.bookreview.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 
-    return jsonify({'msg':'저장이 완료되었습니다!'})
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    reviews = list(db.bookreview.find({}, {'_id': False}))
+    return jsonify({'all_reviews': reviews})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
